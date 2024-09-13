1f6acc66c044e192196d866acb2a26bd9476fb84
Narcio Silvestre <a21723@alunos.ipca.pt>
2024-09-13 06:52:36 +0100

    add new features

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(window.scrollX)
+console.log(window.scrollY)
+const sec2 = document.querySelector(".section_2").getClientRects()
+
+
+/* setInterval(()=>{
+    if(window.scrollY > sec2[0].y) document.querySelector(".nav").style.position="relative"
+    else document.querySelector(".nav").style.position="fixed"
+},1) */
+
+
