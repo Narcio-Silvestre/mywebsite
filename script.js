5977e198b1d5727772ae8a68ada547f3622e97bc
Narcio Silvestre <a21723@alunos.ipca.pt>
2024-09-14 05:33:03 +0100

    adding scrolling

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-console.log(window.scrollX)
-console.log(window.scrollY)
-const sec2 = document.querySelector(".section_2").getClientRects()
+document.querySelector('#moreLink').addEventListener('click',e => scrolling(e))
 
+document.querySelector('.logo').addEventListener('click',e =>{
+    e.preventDefault()
+    let sect =  e.currentTarget.getAttribute('href');
+    const node = document.querySelector(`${sect}`)
+    node.scrollIntoView({behavior:"smooth"})
+})
 
-/* setInterval(()=>{
-    if(window.scrollY > sec2[0].y) document.querySelector(".nav").style.position="relative"
-    else document.querySelector(".nav").style.position="fixed"
-},1) */
+document.querySelector('.nav').addEventListener('click',e =>{
+    if(e.target.classList.contains('nav-item')) scrolling(e)
+})
+
+function scrolling(e){
+    e.preventDefault()
+    let sect =  e.target.getAttribute('href');
+    const node = document.querySelector(`${sect}`)
+    node.scrollIntoView({behavior:"smooth"})
+}
 
 
